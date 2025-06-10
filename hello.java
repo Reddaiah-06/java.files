f8585fcade3f15c57232b744d97e05bc0e423f26
Reddaiah-06 <reddaiahyadav51@gmail.com>
2025-06-10 14:28:01 +0530

    Add files via upload

---
+++ hello.java
@@ -0,0 +1,12 @@
+public class hello{
+    public static void main(String [] args){
+        System.out.println("welcome to aits");
+        main("sama");
+    }
+        public static void main(String name)
+        {
+             System.out.println("welcome to aits"+ name);
+
+    }
+}
+        
